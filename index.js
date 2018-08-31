497bbfdbff82df42d34cab114d3ad7bd220420d1
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-08-31 14:30:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ return printBadges;
 };
 function tailsNeverFails(){
   let tails = 0;
-  while(Math.random() => 0.5){
+  while (Math.random() >= 0.5) {
     tails++;
-  };
+  }
   return `You got ${tails} tails in a row!`;
 };
