efa7e14e2aa30528151655ffc412afd4fc701206
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-08-31 14:34:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Code your solutions in this file
-function printBadges(name){
-  for (let i = 0; i < name.length; i++) {
-  console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
+function printBadges (names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
   }
-return name;
-};
+  return names;
+}
 function tailsNeverFails(){
   let tails = 0;
   while (Math.random() >= 0.5) {
